036a40d6e71a2de63716eba5006e6aedb74c1a50
Yeleefff <150309333+Yeleefff@users.noreply.github.com>
2025-06-10 14:26:10 -0400

    Updated ModBlocks

--- src/main/java/net/yeleefff/enderitemod/block/ModBlocks.java
+++ src/main/java/net/yeleefff/enderitemod/block/ModBlocks.java
@@ -1,10 +1,12 @@
 package net.yeleefff.enderitemod.block;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.MapColor;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
@@ -56,8 +58,8 @@ public class ModBlocks {
     public static void registerModBlocks() {
         EnderiteMod.LOGGER.debug("Registering Mod Blocks for " + EnderiteMod.MOD_ID);
 
-//        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> entries.add(ENDERITE_ORE));
-//        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> entries.add(ENDERITE_BLOCK));
-//        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(entries -> entries.add(ENDERITE_ORE));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> entries.add(ENDERITE_ORE));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> entries.add(ENDERITE_BLOCK));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(entries -> entries.add(ENDERITE_ORE));
     }
 }
